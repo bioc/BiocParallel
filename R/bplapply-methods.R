9a02703b2eb9c5324a6d69da05f980aafd5bd600
Martin Morgan <mtmorgan@fhcrc.org>
2013-10-20 16:59:49 +0000

    back-port BatchJobs patch to BiocParallel
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_13/madman/Rpacks/BiocParallel@81897 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bplapply-methods.R
+++ R/bplapply-methods.R
@@ -1,14 +1,16 @@
-setMethod(bplapply, c("ANY", "ANY"),
-    function(X, FUN, ..., BPPARAM)
+setMethod(bplapply, c("ANY", "missing"),
+    function(X, FUN, ..., resume=getOption("BiocParallel.resume", FALSE),
+        BPPARAM)
 {
     FUN <- match.fun(FUN)
-    lapply(X, FUN, ...)
+    x <- registered()[[1L]]
+    bplapply(X, FUN, ..., resume=resume, BPPARAM=x)
 })
 
-setMethod(bplapply, c("ANY", "missing"),
-    function(X, FUN, ..., BPPARAM)
+setMethod(bplapply, c("ANY", "BiocParallelParam"), 
+    function(X, FUN, ..., resume=getOption("BiocParallel.resume", FALSE),
+        BPPARAM)
 {
-    FUN <- match.fun(FUN)
-    x <- registered()[[1]]
-    bplapply(X, FUN, ..., BPPARAM=x)
+    bpmapply(FUN, X, MoreArgs=list(...), SIMPLIFY=FALSE, USE.NAMES=FALSE,
+        resume=resume, BPPARAM=BPPARAM)
 })
