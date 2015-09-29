7386e83890b5ba7e4a890446427d4158613fa86e
Valerie Obenchain <vobencha@fhcrc.org>
2015-09-29 19:01:59 +0000

    modify unit tests for windows
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/BiocParallel@108966 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_errorhandling.R
+++ inst/unitTests/test_errorhandling.R
@@ -13,112 +13,115 @@ checkExceptionText <- function(expr, txt, negate=FALSE, msg="")
 
 test_catching_errors <- function()
 {
-    x <- 1:10
-    y <- rev(x)
-    f <- function(x, y) if (x > y) stop("whooops") else x + y
+    if (.Platform$OS.type != "windows") {
+        x <- 1:10
+        y <- rev(x)
+        f <- function(x, y) if (x > y) stop("whooops") else x + y
 
-    registerDoParallel(2)
-    params <- list(
-        serial=SerialParam(catch.errors=TRUE),
-        snow=SnowParam(2),
-        dopar=DoparParam(),
-        batchjobs=BatchJobsParam(progressbar=FALSE))
-    if (.Platform$OS.type != "windows")
-        params$mc <- MulticoreParam(2)
+        registerDoParallel(2)
+        params <- list(
+            serial=SerialParam(catch.errors=TRUE),
+            snow=SnowParam(2),
+            dopar=DoparParam(),
+            batchjobs=BatchJobsParam(progressbar=FALSE),
+            mc=MulticoreParam(2))
 
-    for (param in params) {
-        res <- bplapply(list(1, "2", 3), sqrt, BPPARAM=param)
-        checkTrue(length(res) == 3L)
-        msg <- "non-numeric argument to mathematical function"
-        checkIdentical(conditionMessage(res[[2]]), msg)
+        for (param in params) {
+            res <- bplapply(list(1, "2", 3), sqrt, BPPARAM=param)
+            checkTrue(length(res) == 3L)
+            msg <- "non-numeric argument to mathematical function"
+            checkIdentical(conditionMessage(res[[2]]), msg)
+            closeAllConnections()
+        }
+
+        ## clean up
+        env <- foreach:::.foreachGlobals
+        rm(list=ls(name=env), pos=env)
         closeAllConnections()
-    }
-
-    ## clean up
-    env <- foreach:::.foreachGlobals
-    rm(list=ls(name=env), pos=env)
-    closeAllConnections()
-    TRUE
+        TRUE
+    } else TRUE
 }
 
 test_BPREDO <- function()
 {
-    f = sqrt
-    x = list(1, "2", 3) 
-    x.fix = list(1, 2, 3) 
+    if (.Platform$OS.type != "windows") {
+        f = sqrt
+        x = list(1, "2", 3) 
+        x.fix = list(1, 2, 3) 
 
-    registerDoParallel(2)
-    params <- list(
-        serial=SerialParam(catch.errors=TRUE),
-        snow=SnowParam(2),
-        dopar=DoparParam(),
-        batchjobs=BatchJobsParam(progressbar=FALSE))
-    if (.Platform$OS.type != "windows")
-        params$mc <- MulticoreParam(2)
+        registerDoParallel(2)
+        params <- list(
+            serial=SerialParam(catch.errors=TRUE),
+            snow=SnowParam(2),
+            dopar=DoparParam(),
+            batchjobs=BatchJobsParam(progressbar=FALSE),
+            mc=MulticoreParam(2))
 
-    for (param in params) {
-        res <- bpmapply(f, x, BPPARAM=param, SIMPLIFY=TRUE)
-        checkTrue(inherits(res[[2]], "condition"))
+        for (param in params) {
+            res <- bpmapply(f, x, BPPARAM=param, SIMPLIFY=TRUE)
+            checkTrue(inherits(res[[2]], "condition"))
+            closeAllConnections()
+            Sys.sleep(0.25)
+
+            ## data not fixed
+            res2 <- bpmapply(f, x, BPPARAM=param, BPREDO=res, SIMPLIFY=TRUE)
+            checkTrue(inherits(res2[[2]], "condition"))
+            closeAllConnections()
+            Sys.sleep(0.25)
+
+            ## data fixed
+            res3 <- bpmapply(f, x.fix, BPPARAM=param, BPREDO=res, SIMPLIFY=TRUE)
+            checkIdentical(res3, sqrt(1:3))
+            closeAllConnections()
+            Sys.sleep(0.25)
+        }
+
+        ## clean up
+        env <- foreach:::.foreachGlobals
+        rm(list=ls(name=env), pos=env)
         closeAllConnections()
-        Sys.sleep(0.25)
-
-        ## data not fixed
-        res2 <- bpmapply(f, x, BPPARAM=param, BPREDO=res, SIMPLIFY=TRUE)
-        checkTrue(inherits(res2[[2]], "condition"))
-        closeAllConnections()
-        Sys.sleep(0.25)
-
-        ## data fixed
-        res3 <- bpmapply(f, x.fix, BPPARAM=param, BPREDO=res, SIMPLIFY=TRUE)
-        checkIdentical(res3, sqrt(1:3))
-        closeAllConnections()
-        Sys.sleep(0.25)
-    }
-
-    ## clean up
-    env <- foreach:::.foreachGlobals
-    rm(list=ls(name=env), pos=env)
-    closeAllConnections()
-    TRUE
+        TRUE
+    } else TRUE
 }
 
 test_bpiterate_errors <- function()
 {
-    quiet <- suppressMessages
-    .lazyCount <- function(count) {
-        count <- count
-        i <- 0L
+    if (.Platform$OS.type != "windows") {
+        quiet <- suppressMessages
+        .lazyCount <- function(count) {
+            count <- count
+            i <- 0L
  
-        function() {
-            if (i >= count)
-                return(NULL)
-            else
-                i <<- i + 1L
+            function() {
+                if (i >= count)
+                    return(NULL)
+                else
+                    i <<- i + 1L
  
-            if (i == 2)
-                "2"
-            else
-                i
+                if (i == 2)
+                    "2"
+                else
+                    i
+            }
         }
-    }
 
-    FUN <- function(count, ...) {
-        if (count == 2)
-            stop("hit error")
-        else count 
-    }
-    params <- list(snow=SnowParam(2, stop.on.error=TRUE))
-    if (.Platform$OS.type != "windows")
-        params$mc <- MulticoreParam(2, stop.on.error=TRUE)
+        FUN <- function(count, ...) {
+            if (count == 2)
+                stop("hit error")
+            else count 
+        }
+        params <- list(snow=SnowParam(2, stop.on.error=TRUE),
+                       mc=MulticoreParam(2, stop.on.error=TRUE))
 
-    for (p in params) {
-        ITER <- .lazyCount(3)
-        quiet(res <- bpiterate(ITER, FUN, BPPARAM=p))
-        checkTrue(is(res[[2]], "condition"))
+        for (p in params) {
+            ITER <- .lazyCount(3)
+            quiet(res <- bpiterate(ITER, FUN, BPPARAM=p))
+            checkTrue(is(res[[2]], "condition"))
+            closeAllConnections()
+        }
+
+        ## clean up
         closeAllConnections()
-    }
-
-    ## clean up
-    closeAllConnections()
-    TRUE
+        TRUE
+    } else TRUE
 }
